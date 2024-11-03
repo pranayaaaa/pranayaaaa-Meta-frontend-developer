7669cabe8155dcbce53268eaa9ba0b5c31df3a2a
Pranaya <pranayapounikar24@gmail.com>
2024-11-04 01:12:21 +0530

    Your commit message

---
+++ meta-frontend-capstone-main/src/layouts/Layout/Layout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Topbar from "../Topbar/Topbar";
+import Footer from "../Footer/Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Topbar />
+      <main data-testid="main-content">{children}</main>
+      <Footer />
+    </>
+  );
+};
+
+export default Layout;
